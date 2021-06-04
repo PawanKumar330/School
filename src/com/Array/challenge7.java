7b99bac1f88d3c8ee83b5f02f2f5c3067ffefe30
Sumant Kumar <78304397+Pawankryadav@users.noreply.github.com>
2021-06-04 10:50:44 -0600

    record breaker

--- src/com/Array/challenge7.java
+++ src/com/Array/challenge7.java
@@ -1,16 +1,27 @@
 public class challenge7 {
+    
     public static void main(String[] args) {
-        int a[]={1,2,0,7,2,0,2,2};
-        for(int i=0;i<a.length-1;i++){
-            int c=0;
-        for(int j=i;j>0;){
-            --j;
-            if((a[i]<a[j]) &&(a[i]<a[i+1]))
-            c++;
- }
- if(c==0)
-        System.out.println(a[i]);
-        }
+        int a[]={1,-1,0,7,2,8,2,9};int d=0;
+        int b[]=new int[a.length];
+       for(int i=a.length-1;i>=0;i--){
+           int c=0;
+           for(int j=i-1;j>=0;j--){
+               if((a[i]>a[j]) &&(i==a.length-1))
+               c++;
+               else if((a[i]>a[j]) && (a[i]>a[i+1]))
+               c++;
+           }
+           if((c==i) &&(i!=0))
+           d++;
+           if(i==0)
+           if(a[i]>a[i+1])
+           d++;
+           
+
+       }
+       System.out.println(d);
+      
+       
         
     }
 }
